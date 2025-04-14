f085b44da583bbcbc49fce68c97980e9579a70a8
Сергей Скворцов <sergejskvorcov122@mail.ru>
2025-04-14 21:51:52 +0300

    Update tree.component.ts

--- libs/tree/src/lib/tree/main-feature/tree-feature/tree.component.ts
+++ libs/tree/src/lib/tree/main-feature/tree-feature/tree.component.ts
@@ -12,11 +12,10 @@ import { Tree } from '@ma/shared';
 export class TreeComponent {
   @Input() nodes: Tree[] = [];
 
-  // Хранение состояния раскрытых узлов по id
+  
   expanded: { [key: number]: boolean } = {};
 
   toggle(node: Tree): void {
-    // Раскрываем узел, если у него есть вложенности
     if (node.children && node.children.length > 0) {
       this.expanded[node.id] = !this.expanded[node.id];
     }
